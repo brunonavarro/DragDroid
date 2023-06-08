0c274620c74111338930e5a68f43e3bb28f696c9
Bruno Navarro <brunonavarro34@gmail.com>
2023-06-08 15:09:22 -0500

    optimize library implements

--- app/src/main/java/com/nsoft/comunityapp/draganddrop/ui/entities/DragItem.kt
+++ app/src/main/java/com/nsoft/comunityapp/draganddrop/ui/entities/DragItem.kt
@@ -3,40 +3,6 @@ package com.nsoft.comunityapp.draganddrop.ui.entities
 import androidx.compose.ui.graphics.Color
 import com.nsoft.comunityapp.dragdroid_kt.interfaces.ItemUIImpl
 
-//interface ItemUI<T> {
-//    var column: Column
-//    var backgroundColor: Color
-//
-//    fun updateItem(
-//        personUIItem: T,
-//        index: Int,
-//        columnPosition: ColumnPosition<Column>,
-//        rowPosition: RowPosition
-//    )
-//}
-
-
-//class PersonUIItemIMPl(override var column: Column, override var backgroundColor: Color) :
-//    ItemUI<DragItem> {
-//    override fun updateItem(
-//        dragItem: DragItem,
-//        index: Int,
-//        columnPosition: ColumnPosition<Column>,
-//        rowPosition: RowPosition
-//    ) {
-//        dragItem.id = index
-//        dragItem.rowPosition.from = index
-//        dragItem.rowPosition.to = rowPosition.to
-//        columnPosition.to?.let {
-//            dragItem.column = it
-//        }
-//        dragItem.columnPosition.from = columnPosition.from
-//        dragItem.columnPosition.to = columnPosition.to
-//        dragItem.isDraggable = false
-//    }
-//
-//}
-
 data class DragItem(
     var name: String,
     override var id: Int = 0,
